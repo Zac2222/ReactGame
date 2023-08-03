e392eb66a57aae39443692dfae2b18f5460a1881
zac2222 <zacrice2222@gmail.com>
2023-08-02 18:29:59 -0700

    first commit, nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
   return(
-    <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={{
+      base: `'nav' 'main'`,
+      lg: `'nav nav' 'aside main'` //1024px
+    }}> 
+     
+      <GridItem area={'nav'} bg={'orange'}>Nav</GridItem>
+      <Show above='lg'>
+        <GridItem area={'aside'} bg={'gold'}>Aside</GridItem>
+      </Show>
+      <GridItem area={'main'} bg={'blue'}>Main</GridItem>
+    </Grid>
   )
 }
 
